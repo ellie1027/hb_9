0bed31b7d5e417604f14ce9827d536ad05be2b17
hanbyul9 <beammeupplzz@gmail.com>
2020-11-16 12:24:40 +0900

    add new blog theme and post

---
+++ gatsby-meta-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: `HB9's BLOG`,
+  description: `Blog posted about ...`,
+  author: `HB9`,
+  introduction: `아는거 모르는거 다 적어버리기`,
+  siteUrl: `https://hb9.github.io/`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `https://github.com/hanbyul9`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'hanbyul9/hanbyul9.github.io', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'hb9',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: 'test', // Add your google analytics tranking ID
+}
